6c8403a20045e863251f0aae727605cf9f29d543
dlguswo333 <dlguswo002@gmail.com>
2022-07-31 23:16:56 +0900

    Config project

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  ignorePatterns: ['*.js'],
+  rules: {
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'space-infix-ops': 'error',
+    'keyword-spacing': 'error',
+    'space-before-function-paren': ['error', 'always'],
+    'arrow-spacing': 'error'
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const process = require('process')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const development = !!process.env.NODE_ENV;
+
+console.log(`Webpack building in ${development ? 'development' : 'production'}`)
+
+module.exports = {
+  entry: __dirname + '/src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname + '/build')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'source-map' : '',
+  devServer: {
+    liveReload: true,
+    hot: true,
+    watchFiles: ['src/**/*'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+};
