927b98dae121f9acd36262c4e46e59f0457968c1
dlguswo333 <dlguswo002@gmail.com>
2022-08-07 20:07:49 +0900

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'space-infix-ops': 'error',
     'keyword-spacing': 'error',
     'space-before-function-paren': ['error', 'always'],
+    'space-before-blocks': ['error', 'always'],
     'arrow-spacing': 'error'
   }
 };
